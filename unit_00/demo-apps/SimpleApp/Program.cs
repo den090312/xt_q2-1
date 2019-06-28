1820e77511c4d088a844470a45a807fc4096ae6c
average.joe <example@example.com>
2019-06-28 15:12:41 +0400

    feat(tasks): Implement solution for prime numbers detection

---
+++ unit_00/demo-apps/SimpleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using AppBase;
+using Simple;
+using System;
+
+namespace SimpleApp
+{
+    /// <summary>
+    /// Implements detection of the fact that a number is a prime number in form of application.
+    /// </summary>
+    internal class Application : GenericConsoleApplication
+    {
+        protected override void Solve()
+        {
+            if (Value < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var result = PrimeNumbersDetector.IsPrime(Value) ? "is prime number." : "is not prime number.";
+            Console.WriteLine("Value {0} {1}", Value, result);
+        }
+    }
+
+    /// <summary>
+    /// Defines an entry point for the .NET Core CLI app.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Starts the Application that detects prime numbers.
+        /// </summary>
+        public static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
---
+++ unit_00/src/Simple/Simple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Simple
+{
+    /// <summary>
+    /// Provides implementation of <see href="https://en.wikipedia.org/wiki/Trial_division">Trial division</see> method.
+    /// Please refer to <see href="https://en.wikipedia.org/wiki/Primality_test">primality test</see> page for additional information.
+    /// </summary>
+    public class PrimeNumbersDetector
+    {
+        /// <summary>Detects <paramref name="n">n</paramref> is a prime number.</summary>
+        /// <param name="n">Number to test.</param>
+        public static bool IsPrime(int n)
+        {
+            if (n <= 3)
+            {
+                return n > 0;
+            }
+            else if (n % 2 == 0 || n % 3 == 0)
+            {
+                return false;
+            }
+
+            var i = 5;
+            var t = Math.Sqrt(n);
+            while (i <= t)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                {
+                    return false;
+                }
+
+                i += 6;
+            }
+            return true;
+        }
+    }
+}
