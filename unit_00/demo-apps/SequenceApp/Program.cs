13817c26620ad8f4daf772da814eb5e90a9059d4
average.joe <example@example.com>
2019-06-28 15:09:21 +0400

    feat(tasks): Implement solution for sequence generation problem

---
+++ unit_00/demo-apps/SequenceApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using AppBase;
+using Sequence;
+using System;
+
+namespace SequenceApp
+{
+    /// <summary>
+    /// Implements an application to generate sequence of numbers based upon user input.
+    /// </summary>
+    internal class Application : GenericConsoleApplication
+    {
+        protected override void Solve()
+        {
+            Console.WriteLine(Generator.MakeSequence(Value));
+        }
+    }
+
+    /// <summary>
+    /// Defines an entry point for the .NET Core CLI app.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Starts the Application to solve Sequence generation problem.
+        /// </summary>
+        public static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
---
+++ unit_00/src/Sequence/Sequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Sequence
+{
+    /// <summary>Represents a generator that creates sequences of integers separated by commas.</summary>
+    public class Generator
+    {
+        /// <summary>Creates string with a sequence of numbers separated by commas that starts with 1 and ends with <paramref name="top">top</paramref>.</summary>
+        /// <param name="top">Last term of the sequence.</param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="top">top</paramref> is less or equals zero.</exception>
+        /// <exception cref="T:System.OutOfMemoryException">Out of memory.</exception>
+        public static string MakeSequence(int top)
+        {
+            if (top <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(top), "Top value must be greater than zero.");
+            }
+
+            return string.Join(", ", Enumerable.Range(1, top));
+        }
+    }
+}
