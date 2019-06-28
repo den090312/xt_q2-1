d6aa8954d878c91ed996cb2a48353cbef02bdf26
average.joe <example@example.com>
2019-06-28 15:10:27 +0400

    feat(tasks): Implement solution for square plotting problem

---
+++ unit_00/demo-apps/SquareApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using AppBase;
+using Square;
+
+namespace SquareApp
+{
+    /// <summary>
+    /// Implements the plotter of squares.
+    /// </summary>
+    internal class Application : GenericConsoleApplication
+    {
+        protected override void Solve()
+        {
+            new Plotter().PlotSquare(Value);
+        }
+    }
+
+    /// <summary>
+    /// Defines an entry point for the .NET Core CLI app.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Starts the Application that plots a square.
+        /// Square with a size of 1 is a whitespace.
+        /// </summary>
+        public static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
---
+++ unit_00/src/Square/Square.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace Square
+{
+    /// <summary>Provides functionality to create square with a space in center via StringBuilder.</summary>
+    public class Plotter
+    {
+        private readonly StringBuilder _stringBuilder = new StringBuilder("", 255);
+
+        /// <summary>Creates string that holds a square with a space in center.</summary>
+        /// <param name="sideSize">Size of the square's side.</param>
+        /// <exception cref="T:System.ArgumentException"><paramref name="sideSize">sideSize</paramref> is less than or equal to zero or even.</exception>
+        public string MakeSquare(int sideSize)
+        {
+            if (sideSize <= 0 || sideSize % 2 == 0)
+            {
+                throw new ArgumentException($"Invalid value of sideSize = {sideSize}.", nameof(sideSize));
+            }
+
+            if (sideSize == 1)
+            {
+                return " ";
+            }
+
+            _stringBuilder.Clear();
+
+            AppendLines(sideSize);
+
+            _stringBuilder[GetCenterIndex(sideSize)] = ' ';
+
+            return _stringBuilder.ToString();
+        }
+
+        private static int GetCenterIndex(int sideSize)
+        {
+            var half = sideSize / 2;
+            return sideSize * (half + 1) + (half - 1);
+        }
+
+        private void AppendLines(int count)
+        {
+            for (var i = 0; i < count; ++i)
+            {
+                _stringBuilder.Append('*', count);
+                _stringBuilder.AppendLine();
+            }
+        }
+
+        /// <summary>Outputs square with size<paramref name="sideSize">sideSize</paramref> and space in center to console.</summary>
+        /// <param name="sideSize">Size of the square's side.</param>
+        public void PlotSquare(int sideSize)
+        {
+            try
+            {
+                Console.Write(MakeSquare(sideSize));
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Unable to create square due to: {ex.Message}");
+            }
+        }
+    }
+}
