d210df0f65ddf91945256d1fdd39c9f75d5a3359
average.joe <example@example.com>
2019-06-29 17:58:38 +0400

    feat(tasks): Add Triangle task implementation

---
+++ unit_01/demo-apps/Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationBase;
+using System;
+using UtilsLibrary;
+
+namespace Triangle
+{
+    /// <summary>
+    /// Implements printer for left aligned right triangles.
+    /// </summary>
+    internal class Application : IntReader
+    {
+        protected override void PrintIntro()
+            => Console.WriteLine("Welcome to unit 01 - Triangle. User input represents the height of triangle.");
+
+        protected override void Solve()
+        {
+            if (Value < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            new TrianglePrinter().PrintRightTriangle(Value);
+        }
+    }
+
+    /// <summary>
+    /// Defines an entry point for the .NET Core CLI app.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
