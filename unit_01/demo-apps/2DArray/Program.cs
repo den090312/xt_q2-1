7ecb102ccdd19f7a7830db1d3ae9084f0886cecf
average.joe <example@example.com>
2019-06-29 20:25:47 +0400

    feat(tasks): Add implementation of summator for even-positioned elements in 2d array

---
+++ unit_01/demo-apps/2DArray/Program.cs
@@ -0,0 +1,59 @@
+﻿using UtilsLibrary;
+using System;
+
+namespace _2DArray
+{
+    internal class Application
+    {
+        private const int Width = 4;
+        private const int Height = 4;
+
+        private readonly int[,] _values = new int[Width, Height];
+
+        public void Run()
+        {
+            PopulateArray();
+            PrintIntro();
+            Solve();
+        }
+
+        private void PopulateArray()
+        {
+            var random = new Random();
+            for (var i = 0; i < _values.GetLength(0); ++i)
+            {
+                for (var j = 0; j < _values.GetLength(1); ++j)
+                {
+                    _values[i, j] = random.Next(0, 10);
+                }
+            }
+        }
+
+        private void PrintIntro()
+        {
+            Console.WriteLine("This is the demo application for 2D Array sum utility");
+            Console.WriteLine("Here is an array:");
+            for (var i = 0; i < _values.GetLength(0); ++i)
+            {
+                for (var j = 0; j < _values.GetLength(1); ++j)
+                {
+                    Console.Write("{0} ", _values[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void Solve()
+        {
+            Console.WriteLine("Sum of even-positioned elements of this array is: {0}", ArrayUtils.SumEventPositionValues(_values));
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
