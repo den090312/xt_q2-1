a18a2fe896213df10b1615f8d2b37977dcc7e01c
average.joe <example@example.com>
2019-06-29 17:59:35 +0400

    feat(tasks): Add "Another Triangle" task implementation

---
+++ unit_01/demo-apps/AnotherTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ApplicationBase;
+using UtilsLibrary;
+
+namespace AnotherTriangle
+{
+    /// <summary>
+    /// Implements printer for isosceles triangle.
+    /// </summary>
+    internal class Application : IntReader
+    {
+        protected override void PrintIntro()
+            => Console.WriteLine("Welcome to unit 01 - \"Another Triangle\" application. User input represents the height of triangle.");
+
+        protected override void Solve()
+        {
+            if (Value < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            new TrianglePrinter().PrintIsoscelesTriangle(Value);
+        }
+    }
+
+    /// <summary>
+    /// Defines an entry point for the .NET Core CLI app.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            new Application().Run();
+        }
+    }
+}
