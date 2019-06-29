c2eac37e27892dc74f692f3b394807d9e26f1f08
average.joe <example@example.com>
2019-06-29 18:43:52 +0400

    feat(tasks): Add implementation for Char Doubler task

---
+++ unit_01/demo-apps/CharDoubler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UtilsLibrary;
+
+namespace CharDoubler
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Welcome to Char Doubler application.");
+            Console.WriteLine("Please input first string below:");
+            var first = Console.ReadLine();
+            Console.WriteLine("Please input second string below:");
+            var second = Console.ReadLine();
+            var result = StringUtils.IncreaseCharactersMatchingSecondString(first, second);
+            Console.WriteLine("The result is:");
+            Console.WriteLine(result);
+        }
+    }
+}
