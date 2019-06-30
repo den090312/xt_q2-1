c70ed99c9f81426331f18623a274f88220f0bd6b
average.joe <example@example.com>
2019-06-30 13:45:16 +0400

    feat(tasks): Add rectangle utility to the library

---
+++ unit_01/src/UtilsLibrary/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace UtilsLibrary
+{
+    public class Rectangle
+    {
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public BigInteger Area => Width * Height;
+    }
+}
